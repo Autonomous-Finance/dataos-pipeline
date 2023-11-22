2116073cbdcdc63e8dd6f7b85db35454a15659d1
smowden <smowden@users.noreply.github.com>
2023-11-22 13:32:46 +0100

    add env name env file

--- deployments.py
+++ deployments.py
@@ -1,7 +1,9 @@
+import os
+
 from prefect import serve
 from flows import fetch_files_from_arweave
 
 if __name__ == "__main__":
-    fetch_files_from_arweave_deploy = fetch_files_from_arweave.to_deployment(name="downloader")
+    fetch_files_from_arweave_deploy = fetch_files_from_arweave.to_deployment(name=os.environ['DEPLOYMENT_NAME'])
     # fast_deploy = fast_flow.to_deployment(name="fast")
     serve(fetch_files_from_arweave_deploy) # add flows as args here
